21ca036c17e058d79dcfb77d08a20bad65481793
Mingqian Wang <mingqianwang96@gmail.com>
2015-11-03 01:37:36 +0000

    Created articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'pages#home'
-
+  get 'about', to: 'pages#about'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   
-  get 'about', to: 'pages#about'
+  resources :articles
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase

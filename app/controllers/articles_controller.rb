c187407a829a912e76e91928b1864bc6dc959926
Mingqian Wang <mingqianwang96@gmail.com>
2015-11-03 15:44:01 +0000

    Created show page to display newly created articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,12 +7,17 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "The article was successfully saved"
+      flash[:notice] = "The article was successfully created"
       redirect_to article_path(@article)
     else
       render 'new'
     end
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
 
   private
     def article_params

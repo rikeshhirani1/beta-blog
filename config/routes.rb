33ddafd4e7150c9e4aa6691a5bf46292fff5f401
Mingqian Wang <mingqianwang96@gmail.com>
2015-11-04 12:59:20 +0000

    Added users controller, sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,10 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  
+
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase

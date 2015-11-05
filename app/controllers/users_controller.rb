11e5316c7345db9da03599fe55ba5d8838f4fc92
Mingqian Wang <mingqianwang96@gmail.com>
2015-11-05 13:16:21 +0000

    Added production group

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to To-do list #{@user.username}"
-      redirect_to articles_path
+      
+      redirect_to login_path
     else
       render 'new'
     end
